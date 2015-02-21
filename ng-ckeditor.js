681a984e6663c3e784bcf9e60dfea6f32647429b
Francesco Portus <francesco.portus@scai.it>
2015-02-21 12:46:17 +0100

    Pull Request: #46

--- ng-ckeditor.js
+++ ng-ckeditor.js
@@ -83,9 +83,9 @@
                         configLoaderDef = $q.defer();
 
                     element.bind('$destroy', function () {
-                        instance.destroy(
-                            false //If the instance is replacing a DOM element, this parameter indicates whether or not to update the element with the instance contents.
-                        );
+                        if (instance && CKEDITOR.instances[instance.name]) {
+                            CKEDITOR.instances[instance.name].destroy();
+                        }
                     });
                     var setModelData = function (setPristine) {
                         var data = instance.getData();
